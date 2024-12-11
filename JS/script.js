e00cb0604c4aae348fc186fd1c9d9c497010f021
DimitarGateshki <101872373+DimitarGateshki@users.noreply.github.com>
2024-12-11 02:16:11 +0200

    Finishing

--- JS/script.js
+++ JS/script.js
@@ -55,3 +55,53 @@ document.addEventListener('DOMContentLoaded', function() {
 
 })
 
+
+document.addEventListener('DOMContentLoaded', function() {
+
+// Set the date we're counting down to
+    var countDownDate = new Date("Dec 11, 2024 01:56:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    if (distance <= 0) {
+        document.getElementById("demo").innerHTML = "Eggstra is launched ," + "<span>" + " BUY NOW !!!"+ "</span>";
+        clearInterval(x); 
+    }else{
+
+        console.log('wtf')
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("demo").innerHTML = "Get ready for the official EGGstra launch! Release in: <span>" + days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s " + "</span>";
+
+        var buyBTN = document.getElementById("buy");
+        document.getElementById("buyBTN").classList.add("disabled")
+        buyBTN.classList.add("disabled");
+        buyBTN.innerHTML = 'Coming soon';
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            document.getElementById("demo").innerHTML = "Eggstra is launched ," + "<span>" + " BUY NOW !!!"+ "</span>";
+            document.getElementById("buyBTN").classList.remove("disabled")
+            buyBTN.classList.remove("disabled");
+            buyBTN.innerHTML = 'GET EGGSTRA';
+            clearInterval(x);
+
+        }
+    }
+    }, 100);
+    
+});
+
